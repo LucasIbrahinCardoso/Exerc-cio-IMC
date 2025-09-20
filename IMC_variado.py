66c1516893d88470e32f240cf6ec9e90bf7d3c9b
LucasIbrahinCardoso <lucasibraca@gmail.com>
2025-09-20 14:07:02 -0400

    Commit com problema

--- IMC_variado.py
+++ IMC_variado.py
@@ -1,15 +1,42 @@
 ## Se estiver bagunçado é pq eu fiquei com preguiça de arrumar depois que terminei.
 ## Falando isso desde o início pra pedir desculpas de antemão, professor. KKKKKKKKK.
 
+lista = []
 repetição = True
 while repetição:
-    nome = input("Qual o seu nome? ")
-    peso = float(input("Qual o seu peso? "))
-    altura = float(input("Qual a sua altura? "))
+    nome = input("Digite seu nome: ")
+    if nome == "parar":
+        break
+    lista.append(nome)
+
+    peso = (input("Digite seu peso: "))
+    if peso == "parar":
+        lista.remove(lista[-1])
+        lista.remove(lista[-1])
+    if peso == "parar":
+        break
+    lista.append(peso)
+
+    altura = (input("Digite sua altura: "))
+    if altura == "parar":
+        lista.remove(lista[-1])
+        lista.remove(lista[-1])
+
+
+    if altura == "parar":
+        break
+    lista.append(altura)
+
+    peso = float(peso)
+    altura = float(altura)
 
     imc = peso / (altura * altura)
+    lista.append(imc)
+
     print(f"O IMC de {nome} é {imc:.2f}")
+    
 
 
-    if nome == "parar" or peso == "parar" or altura == "parar":
-        repetição = False
+
+
+print(lista)
