28e7d725671722d386c293d4d5805d5b22560f03
LucasIbrahinCardoso <lucasibraca@gmail.com>
2025-09-22 10:34:08 -0400

    Agora sim

--- IMC_variado.py
+++ IMC_variado.py
@@ -1,6 +1,20 @@
 lista = []
-repetição = True
-while repetição:
+tipos = ["Baixo peso", "Peso normal", "Sobrepeso", "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3"]
+
+def testador(coiso):
+    if coiso < 18.5:
+        return 0
+    if coiso >= 18.5 and coiso < 25:
+        return 1
+    if coiso >= 25 and coiso < 30:
+        return 2
+    if coiso >= 30 and coiso < 35:
+        return 3
+    if coiso >= 35 and coiso < 40:
+        return 4
+    if coiso >= 40:
+        return 5
+while True:
     nome = input("Digite seu nome: ")
     if nome == "parar":
         break
@@ -26,6 +40,10 @@ while repetição:
 
     imc = peso / (altura * altura)
     lista.append(imc)
-    
-for i in range(0, len(lista), 4):
-    print(f"{lista[i]} tem IMC de {lista[i+3]}")
+for l in range(0, len(lista), 4):
+    if l == 0:
+        qual_imc = testador(lista[3])
+        print(f"O IMC de {lista[l]} é {tipos[qual_imc]}")
+    else:
+        qual_imc = testador(lista[l + 3])
+        print(f"O IMC de {lista[l]} é {tipos[qual_imc]}")
